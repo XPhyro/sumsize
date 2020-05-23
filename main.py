b7aa9c5a470e7f935f74d27c650f3fabedf2edde
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2020-05-23 18:46:17 +0300

    Add column and input options

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ import re
 
 UNITS = [ "k", "m", "g", "t", "p" ]
 
+def col(val):
+    ival = int(val)
+    if ival <= 0:
+        raise argparse.ArgumentTypeError(f"{ val } is not a valid column value.")
+    return ival
+
 parser = argparse.ArgumentParser(description="Sum sizes.")
-parser.add_argument("-b", "--useb", help="use B instead of iB", action="store_true")
-parser.add_argument("-f", "--figure", help="significant figure count after the dot", type=int)
+parser.add_argument("-b", "--useb", help="print in B instead of iB", action="store_true")
+parser.add_argument("-u", "--unitless", help="total size is given unitless", action="store_true")
+parser.add_argument("-f", "--figure", help="significant figure count after the dot (default 2)", type=int)
+parser.add_argument("-c", "--column", help="which column to consider as sizes (default 1)", type=col, default=1)
+parser.add_argument("-i", "--input", help="read from file instead of stdin")
 
 args = parser.parse_args()
 
@@ -39,7 +48,11 @@ def sumsize(sizes):
     return s
 
 
-def formatsize(size, F=2, B=False):
+def formatsize(size, F=2, B=False, U=False):
+    size = int(size)
+    if U:
+        return str(size)
+
     c = 1000 if B else 1024
     for i, j in reversed(list(enumerate(UNITS))):
         m = size / (c ** (i + 1))
@@ -49,15 +62,21 @@ def formatsize(size, F=2, B=False):
     return f"{ size }B"
 
 
-stdinLines = stdin.readlines()
-size = sumsize(stdinLines)
+if args.input is None:
+    inputText = stdin.readlines()
+else:
+    with open(args.input, "r") as f:
+        inputText = f.readlines()
+
+sin = [re.split("\s", i)[args.column - 1] for i in inputText]
+
+size = sumsize(sin)
 
 useB = args.useb
-
 figure = 2
 if args.figure is not None:
     figure = args.figure 
 
-f = formatsize(size, F=figure, B=useB)
+formattedSize = formatsize(size, F=figure, B=useB, U=args.unitless)
 
-print(f)
+print(formattedSize)
